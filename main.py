e37436fa1070608c6ec43dd9abad289bc724cb87
bu128 <bunyodbekabdumutalo00@gmail.com>
2026-03-18 15:38:19 +0500

    Update bot token and fix formatting issues

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = ""
+TOKEN = "Bot_token"
 
 # /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
